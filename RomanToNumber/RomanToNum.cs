60b737fe1a113e2f5a882e6fbd9c1688310cf3d3
Kuangzhang <kuangzhang@passport.com>
2022-07-10 11:31:25 -0400

    Created a Console App + Class Lib For Roman To Num Class

---
+++ RomanToNumber/RomanToNum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomanToNumber
+{
+    public class RomanToNum
+    {
+
+
+
+        public static int FindValue(char _Chr)
+        {
+            switch (_Chr)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: return 0;
+            }
+        }
+    }
+}
