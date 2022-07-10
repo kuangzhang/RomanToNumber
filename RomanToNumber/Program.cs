accc540b20ed3f3219475e152fa2a4959643b1a5
Kuangzhang <kuangzhang@passport.com>
2022-07-10 11:28:36 -0400

    Add project files.

---
+++ RomanToNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RomanToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int FindValue(char _Chr)
+        {
+            switch (_Chr)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: return 0;
+            }
+        }
+
+    }
+}
